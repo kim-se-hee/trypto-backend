0f3c03eedf63da5873458597acefa736cc3361fc
kim-se-hee <saree98@naver.com>
2026-03-03 20:49:08 +0900

    feat: marketdata ExchangeCoin 쿼리 추가 - findByExchangeIdAndCoinId 추가

--- src/main/java/ksh/tryptobackend/marketdata/adapter/out/repository/ExchangeCoinJpaRepository.java
+++ src/main/java/ksh/tryptobackend/marketdata/adapter/out/repository/ExchangeCoinJpaRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface ExchangeCoinJpaRepository extends JpaRepository<ExchangeCoinJpaEntity, Long> {
 
     List<ExchangeCoinJpaEntity> findByIdIn(Collection<Long> ids);
+
+    Optional<ExchangeCoinJpaEntity> findByExchangeIdAndCoinId(Long exchangeId, Long coinId);
 }
