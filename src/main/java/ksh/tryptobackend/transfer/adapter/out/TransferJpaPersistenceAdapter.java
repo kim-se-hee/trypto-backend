7fe6bd1f422337971972eefe0ac453cc341be022
kim-se-hee <saree98@naver.com>
2026-03-04 06:39:53 +0900

    feat: 송금 내역 커서 기반 조회 QueryDSL 구현

--- src/main/java/ksh/tryptobackend/transfer/adapter/out/TransferJpaPersistenceAdapter.java
+++ src/main/java/ksh/tryptobackend/transfer/adapter/out/TransferJpaPersistenceAdapter.java
@@ -1,5 +1,8 @@
 package ksh.tryptobackend.transfer.adapter.out;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import ksh.tryptobackend.transfer.adapter.out.entity.QTransferJpaEntity;
 import ksh.tryptobackend.transfer.adapter.out.entity.TransferJpaEntity;
 import ksh.tryptobackend.transfer.adapter.out.repository.TransferJpaRepository;
 import ksh.tryptobackend.transfer.application.port.out.TransferPersistencePort;
@@ -17,6 +20,7 @@ import java.util.UUID;
 public class TransferJpaPersistenceAdapter implements TransferPersistencePort {
 
     private final TransferJpaRepository repository;
+    private final JPAQueryFactory queryFactory;
 
     @Override
     public Transfer save(Transfer transfer) {
@@ -33,6 +37,31 @@ public class TransferJpaPersistenceAdapter implements TransferPersistencePort {
 
     @Override
     public List<Transfer> findByCursor(Long walletId, TransferType type, Long cursorTransferId, int size) {
-        throw new UnsupportedOperationException("QueryDSL 구현 예정");
+        QTransferJpaEntity transfer = QTransferJpaEntity.transferJpaEntity;
+
+        return queryFactory
+            .selectFrom(transfer)
+            .where(
+                walletCondition(transfer, walletId, type),
+                cursorLt(transfer, cursorTransferId)
+            )
+            .orderBy(transfer.id.desc())
+            .limit(size)
+            .fetch()
+            .stream()
+            .map(TransferJpaEntity::toDomain)
+            .toList();
+    }
+
+    private BooleanExpression walletCondition(QTransferJpaEntity transfer, Long walletId, TransferType type) {
+        return switch (type) {
+            case DEPOSIT -> transfer.toWalletId.eq(walletId);
+            case WITHDRAW -> transfer.fromWalletId.eq(walletId);
+            case ALL -> transfer.fromWalletId.eq(walletId).or(transfer.toWalletId.eq(walletId));
+        };
+    }
+
+    private BooleanExpression cursorLt(QTransferJpaEntity transfer, Long cursorTransferId) {
+        return cursorTransferId != null ? transfer.id.lt(cursorTransferId) : null;
     }
 }
