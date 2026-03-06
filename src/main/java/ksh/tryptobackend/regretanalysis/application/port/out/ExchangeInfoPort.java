e2827603be767a999da051eac03bc657f57f2623
kim-se-hee <saree98@naver.com>
2026-03-07 04:50:18 +0900

    refactor: regretanalysis 미사용 구 Output Port 파일 삭제

--- src/main/java/ksh/tryptobackend/regretanalysis/application/port/out/ExchangeInfoPort.java
+++
@@ -1,8 +0,0 @@
-package ksh.tryptobackend.regretanalysis.application.port.out;
-
-import ksh.tryptobackend.regretanalysis.application.port.out.dto.ExchangeInfoRecord;
-
-public interface ExchangeInfoPort {
-
-    ExchangeInfoRecord getExchangeInfo(Long exchangeId);
-}
