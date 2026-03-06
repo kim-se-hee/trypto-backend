8ad34460546777fd8f38e01da9f20afd1cbfeff8
kim-se-hee <saree98@naver.com>
2026-03-07 03:58:26 +0900

    refactor: regretanalysis 어댑터 marketdata UseCase 마이그레이션

--- src/main/java/ksh/tryptobackend/regretanalysis/adapter/out/ExchangeInfoAdapter.java
+++ src/main/java/ksh/tryptobackend/regretanalysis/adapter/out/ExchangeInfoAdapter.java
@@ -2,8 +2,8 @@ package ksh.tryptobackend.regretanalysis.adapter.out;
 
 import ksh.tryptobackend.common.exception.CustomException;
 import ksh.tryptobackend.common.exception.ErrorCode;
-import ksh.tryptobackend.marketdata.application.port.out.ExchangeQueryPort;
-import ksh.tryptobackend.marketdata.application.port.out.dto.ExchangeDetail;
+import ksh.tryptobackend.marketdata.application.port.in.FindExchangeDetailUseCase;
+import ksh.tryptobackend.marketdata.application.port.in.dto.result.ExchangeDetailResult;
 import ksh.tryptobackend.regretanalysis.application.port.out.ExchangeInfoPort;
 import ksh.tryptobackend.regretanalysis.application.port.out.dto.ExchangeInfoRecord;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +13,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ExchangeInfoAdapter implements ExchangeInfoPort {
 
-    private final ExchangeQueryPort exchangeQueryPort;
+    private final FindExchangeDetailUseCase findExchangeDetailUseCase;
 
     @Override
     public ExchangeInfoRecord getExchangeInfo(Long exchangeId) {
-        ExchangeDetail detail = exchangeQueryPort.findExchangeDetailById(exchangeId)
+        ExchangeDetailResult detail = findExchangeDetailUseCase.findExchangeDetail(exchangeId)
             .orElseThrow(() -> new CustomException(ErrorCode.EXCHANGE_NOT_FOUND));
 
         return new ExchangeInfoRecord(exchangeId, detail.name(), detail.domestic() ? "KRW" : "USDT");
