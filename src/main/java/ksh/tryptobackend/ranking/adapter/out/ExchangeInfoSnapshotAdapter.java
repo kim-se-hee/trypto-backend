7f67ced64dc1a3fd0dad97c16c3a66434ddf6e98
kim-se-hee <saree98@naver.com>
2026-03-07 03:58:21 +0900

    refactor: ranking 어댑터 marketdata UseCase 마이그레이션

--- src/main/java/ksh/tryptobackend/ranking/adapter/out/ExchangeInfoSnapshotAdapter.java
+++ src/main/java/ksh/tryptobackend/ranking/adapter/out/ExchangeInfoSnapshotAdapter.java
@@ -2,9 +2,8 @@ package ksh.tryptobackend.ranking.adapter.out;
 
 import ksh.tryptobackend.common.exception.CustomException;
 import ksh.tryptobackend.common.exception.ErrorCode;
-import ksh.tryptobackend.marketdata.application.port.out.ExchangeQueryPort;
-import ksh.tryptobackend.marketdata.application.port.out.dto.ExchangeDetail;
-import ksh.tryptobackend.marketdata.domain.model.ExchangeMarketType;
+import ksh.tryptobackend.marketdata.application.port.in.FindExchangeDetailUseCase;
+import ksh.tryptobackend.marketdata.application.port.in.dto.result.ExchangeDetailResult;
 import ksh.tryptobackend.ranking.application.port.out.ExchangeInfoQueryPort;
 import ksh.tryptobackend.ranking.application.port.out.dto.ExchangeSnapshotInfo;
 import ksh.tryptobackend.ranking.domain.vo.KrwConversionRate;
@@ -15,20 +14,13 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ExchangeInfoSnapshotAdapter implements ExchangeInfoQueryPort {
 
-    private final ExchangeQueryPort exchangeQueryPort;
+    private final FindExchangeDetailUseCase findExchangeDetailUseCase;
 
     @Override
     public ExchangeSnapshotInfo getExchangeInfo(Long exchangeId) {
-        ExchangeDetail detail = exchangeQueryPort.findExchangeDetailById(exchangeId)
+        ExchangeDetailResult detail = findExchangeDetailUseCase.findExchangeDetail(exchangeId)
             .orElseThrow(() -> new CustomException(ErrorCode.EXCHANGE_NOT_FOUND));
-        KrwConversionRate rate = toConversionRate(detail.marketType());
+        KrwConversionRate rate = detail.domestic() ? KrwConversionRate.DOMESTIC : KrwConversionRate.OVERSEAS;
         return new ExchangeSnapshotInfo(exchangeId, detail.baseCurrencyCoinId(), rate);
-    }
-
-    private KrwConversionRate toConversionRate(ExchangeMarketType marketType) {
-        return switch (marketType) {
-            case DOMESTIC -> KrwConversionRate.DOMESTIC;
-            case OVERSEAS -> KrwConversionRate.OVERSEAS;
-        };
     }
 }
