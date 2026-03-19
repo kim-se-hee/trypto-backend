9185891af8cc23f49fe94a3e485c3239f1d9bafb
kim-se-hee <saree98@naver.com>
2026-03-19 16:52:47 +0900

    feat: JPA 엔티티 및 리포지토리 확장 - CoinJpaEntity, ExchangeCoinJpaEntity: @Immutable 제거, @GeneratedValue 추가, 생성자 추가 - ExchangeJpaEntity: 생성자 추가 - CoinJpaRepository: findBySymbol 추가, ExchangeJpaRepository: findByName 추가

--- src/main/java/ksh/tryptobackend/marketdata/adapter/out/repository/CoinJpaRepository.java
+++ src/main/java/ksh/tryptobackend/marketdata/adapter/out/repository/CoinJpaRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface CoinJpaRepository extends JpaRepository<CoinJpaEntity, Long> {
 
     List<CoinJpaEntity> findByIdIn(Collection<Long> ids);
+
+    Optional<CoinJpaEntity> findBySymbol(String symbol);
 }
--- src/main/java/ksh/tryptobackend/marketdata/adapter/out/repository/ExchangeJpaRepository.java
+++ src/main/java/ksh/tryptobackend/marketdata/adapter/out/repository/ExchangeJpaRepository.java
@@ -3,5 +3,9 @@ package ksh.tryptobackend.marketdata.adapter.out.repository;
 import ksh.tryptobackend.marketdata.adapter.out.entity.ExchangeJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ExchangeJpaRepository extends JpaRepository<ExchangeJpaEntity, Long> {
+
+    Optional<ExchangeJpaEntity> findByName(String name);
 }
